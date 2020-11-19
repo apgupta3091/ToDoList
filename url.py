e3e9edfb3234df17f3345bd510aff615665ab46d
Anuj Gupta <53449807+apgupta3091@users.noreply.github.com>
2020-11-18 23:40:05 -0500

    Update url.py

--- url.py
+++ url.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from todo_list import views
 
 urlpatterns = [
+    #path to the admin site for todo_list
     path('admin/', admin.site.urls),
+    #path to the home page for todo_list
     path('', include('todo_list.urls')),
 ]
