785746c6ead865d4ba7060dc5df0eea8a62e0727
Anuj Gupta <53449807+apgupta3091@users.noreply.github.com>
2020-11-18 23:31:35 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import List
 
+#Creates the class ListForm wich takes instanitaties that class Meta
+#The Meta class sets the var model = List with the fields "item, and completed"
 class ListForm(forms.ModelForm):
 	class Meta:
 		model = List
