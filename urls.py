fa8774f35d6f3dd725bba240a95739979ff4b159
Anuj Gupta <53449807+apgupta3091@users.noreply.github.com>
2020-11-18 23:41:57 -0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	#path to the home page
 	path('', views.home, name="home"),
+	#path to the delete function
 	path('delete/<list_id>', views.delete, name="delete"),
+
+    #path to cross off function
     path('cross_off/<list_id>', views.cross_off, name="cross_off"),
+    #path to uncross function
     path('uncross/<list_id>', views.uncross, name="uncross"),
+    #path to the edit page
     path('edit/<list_id>', views.edit, name="edit"),
 ]
